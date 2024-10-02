d32d0abd2cde0e25ff17fd8c26a2106bb9f74edc
Melanie <m1114m1127@gmail.com>
2024-10-03 00:42:17 +0800

    output file

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import os
 
 for i in range(1,11):
   input_file_path = f"input/input{i}.json"; 
-  output_file_path = f"input/output{i}.json"; 
+  output_file_path = f"output/output{i}.json"; 
   
   with open(input_file_path, mode='r') as input_file:
     data = json.load(input_file); 
-    print(data); 
+    
+  with open(output_file_path, mode='w') as output_file:
+    json.dump(data, output_file); 
