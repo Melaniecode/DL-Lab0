18eeb4ff81f3dfb433791bc4a8de33ca76559e83
Melanie <m1114m1127@gmail.com>
2024-10-06 20:34:16 +0800

    Lab 0-1

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import json
 import os
+import numpy as np
 
-for i in range(1,11):
-  input_file_path = f"input/input{i}.json"; 
-  output_file_path = f"output/output{i}.json"; 
-  
-  with open(input_file_path, mode='r') as input_file:
-    data = json.load(input_file); 
-    
-  with open(output_file_path, mode='w') as output_file:
-    json.dump(data, output_file); 
+for i in range(1, 11):
+    input_file_path = f"input/input{i}.json"; 
+    output_file_path = f"output/output{i}.json"; 
+
+    with open(input_file_path, mode='r') as input_file:
+        data = json.load(input_file); 
+
+    result_matrix = np.array(data[0]['matrix']); 
+
+    for datum in data[1:]:
+        matrix = np.array(datum['matrix']); 
+        result_matrix = np.dot(result_matrix, matrix) % 100000; 
+
+    result_matrix = result_matrix.tolist(); 
+
+    with open(output_file_path, mode='w') as output_file:
+        json.dump(result_matrix, output_file); 
